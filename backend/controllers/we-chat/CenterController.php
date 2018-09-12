d5ba5916932823ff904908301e3f0b59a7002ad6
Yxl <zccem@163.com>
2018-09-12 17:12:19 +0800

    沃噻主机上传

--- backend/controllers/we-chat/CenterController.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by Yxl.
- * User: <zccem@163.com>.
- * Date: 2018/8/31
- * Time: 15:38
- */
---
+++ backend/controllers/wechat/CenterController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by Yxl.
+ * User: <zccem@163.com>.
+ * Date: 2018/8/31
+ * Time: 15:38
+ */
+
+namespace backend\controllers\wechat;
+
+use Yii;
+use yii\helpers\Json;
+
+class CenterController extends BaseController
+{
+
+}
